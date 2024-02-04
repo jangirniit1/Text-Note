b352277512ef5dfa1e3884eedee68dd2e57fdfe8
jangirniit1 <jangirniit1@gmail.com>
2024-02-04 13:29:48 +0530

    The noteEle is removable, and the boxes do not overflow from the main box.

---
+++ script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", addNote);
+
+function addNote() {
+  const text = document.getElementById("noteText").value;
+  const color = document.getElementById("colorPicker").value;
+  const noteDisplay = document.getElementById("noteDisplay");
+  const noteEle = document.createElement("div");
+  noteEle.classList.add("noteBox");
+  noteEle.innerHTML = text;
+  noteEle.style.backgroundColor = color;
+
+  const btn2 = document.createElement("a");
+  btn2.classList.add("closeBtn");
+  noteEle.append(btn2);
+  btn2.innerHTML = "&times;";
+
+  const cross = noteEle.querySelector(".closeBtn");
+  cross.addEventListener("click", () => {
+    // noteEle.style.display="none"; // Expected output :- This can remove the div but in the element that div is available.
+    // noteEle.remove();  // Expected output:- This can remove the div from main div.
+    noteDisplay.removeChild(noteEle); // Expected output:- This can remove the div from main div.
+  });
+
+  document.getElementById("noteDisplay").appendChild(noteEle);
+  document.getElementById("noteText").value = "";
+}
